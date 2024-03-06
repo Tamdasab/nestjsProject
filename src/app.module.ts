279cf847777865619d4e99bcd6aeb6d43a66ee93
Sabrina.Tamda <sabrina.tamda@europcar.com>
2024-03-06 16:26:49 +0100

    create logic of application Task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './tasks/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_TASK,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.TASK_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: boolean;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  deadline: Date;
+}
